6d32d0d1f1400da42dfb9f1a79217702ba9f6177
nipun22105074 <121248279+nipun22105074@users.noreply.github.com>
2023-02-02 13:41:44 +0530

    Add files via upload

---
+++ q5.py
@@ -0,0 +1,7 @@
+x=(input('enter the string: '))
+r = int(input("Enter the number of rows: "))
+y, c = 0, 1
+for _ in range(1, 1+r):
+    print(x[y:c])
+    y = c
+    c += _ + 1
---
+++ q7.py
@@ -0,0 +1,4 @@
+
+for i in range(1, 500):
+    if i % 7 == 0 and i % 11 == 0:
+        print(i)
